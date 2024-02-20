f61cc0cd2098db381e263d88216032c140ced2e5
Lu Marconi <lumarconi21@gmail.com>
2024-02-20 11:08:34 +0100

    Add style to Random Page

--- src/components/RandomButton.jsx
+++ src/components/RandomButton.jsx
@@ -1,9 +1,10 @@
+import { LiaDiceSolid } from "react-icons/lia";
 
-
-function RandomButton({onClick}) {
+function RandomButton({ onClick }) {
   return (
-    <div>
-      <button onClick={onClick}>Random Wanderlust Place</button>
+    <div className="flex justify-center items-center h-36">
+      <button onClick={onClick} className="flex gap-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4">
+        <LiaDiceSolid />I'm gonna get lucky</button>
     </div>
   )
 }
--- src/pages/RandomPlace.jsx
+++ src/pages/RandomPlace.jsx
@@ -37,19 +37,34 @@ function RandomPlace() {
 
   return (
     <div>
+      <h2 className="text-center text-2xl font-semibold mt-12">Explore the unknown: Random destination, activate!</h2>
+
       <RandomButton onClick={handleClick} />
 
-      {onePlace && (
-        <>
-          <img src={onePlace.image} style={{ height: "20rem" }} />
-          <h2>{onePlace.city}</h2>
-          <p>{onePlace.country}</p>
-          <p>Activity: {onePlace.activity}</p>
-          <p>Highlight: {onePlace.highlight}</p>
-          <p>{onePlace.textDescription}</p>
-        </>
+      {!onePlace && (
+        <div className="flex justify-center items-center h-96">
+          <h2 className="text-semibold text-2xl">Click the button to start!</h2>
+        </div>
       )}
-    </div>
+
+      {onePlace && (
+        <div className="flex flex-col items-center shadow-lg border-solid border rounded-xl lg:mx-96">
+          <h3 className="text-center text-2xl font-semibold pt-4">{onePlace.city}</h3>
+          <h4 className="text-center text-xl">{onePlace.country}</h4>
+          <div className="flex justify-center">
+            <div>
+              <img src={onePlace.image} className="h-80 w-96 my-12" />
+            </div>
+            <div className="flex flex-col w-96 ml-12 mt-8">
+              <span className="pb-1 mt-4 text-center font-semibold">Activity:</span> <span className="text-center">{onePlace.activity}</span>
+              <span className="py-1 text-center font-semibold">Highlight:</span> <span className="text-center">{onePlace.highlight}</span>
+              <p className="py-4 mb-4 text-center">{onePlace.textDescription}</p>
+            </div>
+          </div>
+        </div>
+      )
+      }
+    </div >
   );
 }
 
