ff11905de4fcd178bfe0e34787a225e4ba858410
Lu Marconi <lumarconi21@gmail.com>
2024-02-20 16:28:04 +0100

    Co-authored-by: MelSchiffner <MelSchiffner@users.noreply.github.com>

--- src/pages/RandomPlace.jsx
+++ src/pages/RandomPlace.jsx
@@ -48,20 +48,24 @@ function RandomPlace() {
       )}
 
       {onePlace && (
-        <div className="flex flex-col items-center shadow-lg border-solid border rounded-xl lg:mx-96">
+        <div className="flex flex-col items-center justify-center shadow-lg border-solid border rounded-xl lg:w-full mx-8">
           <h3 className="text-center text-xl font-semibold lg:text-2xl pt-4">{onePlace.city}</h3>
           <h4 className="text-center text-xl">{onePlace.country}</h4>
-          <div className="flex justify-center">
-            <div>
-              <img src={onePlace.image} className="h-80 w-96 my-12" />
-            </div>
-            <div className="flex flex-col w-96 ml-12 mt-8">
-              <span className="pb-1 mt-4 text-center font-semibold">Activity:</span> <span className="text-center">{onePlace.activity}</span>
-              <span className="py-1 text-center font-semibold">Highlight:</span> <span className="text-center">{onePlace.highlight}</span>
-              <p className="py-4 mb-4 text-center">{onePlace.textDescription}</p>
+          <div className="flex flex-col justify-between lg:flex-row">
+            <div className="flex flex-col w-full lg:w-auto"></div>
+            <div className="flex">
+              <div>
+                <img src={onePlace.image} className="h-80 w-96 my-12 border rounded-lg lg:w-full" />
+              </div>
             </div>
           </div>
+          <div className="flex flex-col lg:w-1/3 mt-8">
+            <span className="pb-1 mt-4 text-center font-semibold">Activity:</span> <span className="text-center">{onePlace.activity}</span>
+            <span className="py-1 text-center font-semibold">Highlight:</span> <span className="text-center">{onePlace.highlight}</span>
+            <p className="py-4 mx-4 mb-4 text-center lg:mx-0 lg:py-8">{onePlace.textDescription}</p>
+          </div>
         </div>
+
       )
       }
     </div >
