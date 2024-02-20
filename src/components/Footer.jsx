99cf903febe864edf4709be10f5bdef269c8ae70
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-20 13:16:52 +0100

    fixed footer permanently to bottom of page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-    <div className="flex bg-green-100  mt-96">
+    <div className="flex bg-green-100 mt-96 sticky bottom-0">
       <div className="w-screen h-8 flex justify-evenly items-center">
         <a href="https://github.com/melucia/melucia-travel-app" target="blank">Melucia ©</a>
       </div>
