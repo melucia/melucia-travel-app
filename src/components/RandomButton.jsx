eb6c65841ad776ba357afda9a145e11fb6dfe575
Lu Marconi <lumarconi21@gmail.com>
2024-02-20 12:20:20 +0100

    Fix some text in About and Random pages

--- src/components/RandomButton.jsx
+++ src/components/RandomButton.jsx
@@ -4,7 +4,7 @@ function RandomButton({ onClick }) {
   return (
     <div className="flex justify-center items-center h-36">
       <button onClick={onClick} className="flex gap-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4">
-        <LiaDiceSolid />I'm gonna get lucky</button>
+        <LiaDiceSolid />I'm feeling lucky</button>
     </div>
   )
 }
