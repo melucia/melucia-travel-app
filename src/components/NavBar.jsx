3feaddfd6cf92be22cb3df0f871e38ac627f7e52
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-21 10:16:24 +0100

    adding redirects file

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     <>
       <div className="flex bg-green-100 top-0 justify-end gap-6 lg:justify-between lg:gap-2">
         <div>
-          <img className="lg:ml-6 mt-1" src="src/assets/melucia-logo.png" atl="logo" />
+          <img className="lg:ml-6 mt-1" src="./assets/melucia-logo.png" atl="logo" />
         </div>
         <nav className="h-16 flex justify-end gap-2 items-center pr-1 lg:justify-evenly  lg:gap-10  lg:pr-8 ">
           <NavLink className="cursor-pointer text-sm   lg:text-xl font-bold  hover:text-blue-800 hover:underline"
