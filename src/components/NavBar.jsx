7c64efbf154b390e1d60a890742de9ff9709c197
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-21 10:31:01 +0100

    fixed logo in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
+import Logo from "../assets/melucia-logo.png";
 
 function NavBar() {
   return (
     <>
       <div className="flex bg-green-100 top-0 justify-end gap-6 lg:justify-between lg:gap-2">
         <div>
-          <img className="lg:ml-6 mt-1" src="./assets/melucia-logo.png" atl="logo" />
+          <img className="lg:ml-6 mt-1" src={Logo} atl="logo" />
         </div>
         <nav className="h-16 flex justify-end gap-2 items-center pr-1 lg:justify-evenly  lg:gap-10  lg:pr-8 ">
           <NavLink className="cursor-pointer text-sm   lg:text-xl font-bold  hover:text-blue-800 hover:underline"
