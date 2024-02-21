5c51110909ae35c062fac2f7dc49b7a91d605908
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-21 14:39:27 +0100

    fixing adding place when reload

--- src/pages/AddPlace.jsx
+++ src/pages/AddPlace.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddPlace({ onAddPlace }) {
   const [newPlace, setNewPlace] = useState({
@@ -10,6 +12,10 @@ function AddPlace({ onAddPlace }) {
     highlight: "",
   });
 
+  const API_URL = "https://melucia-travel-app.adaptable.app";
+
+  const navigate = useNavigate();
+
   const handleValueChanges = (e) => {
     setNewPlace({
       ...newPlace,
@@ -19,33 +25,37 @@ function AddPlace({ onAddPlace }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onAddPlace({
-      id: Date.now(),
-      image: newPlace.image,
-      city: newPlace.city,
-      country: newPlace.country,
-      description: newPlace.description,
-      activity: newPlace.activity,
-      highlight: newPlace.highlight,
-    });
-    setNewPlace({
-      image: "",
-      city: "",
-      country: "",
-      description: "",
-      activity: "",
-      highlight: "",
-    });
+    axios
+      .post(`${API_URL}/places`, newPlace)
+      .then((response) => {
+        console.log("place adding here", response.data);
+        onAddPlace(response.data);
+        setNewPlace({
+          image: "",
+          city: "",
+          country: "",
+          description: "",
+          activity: "",
+          highlight: "",
+        });
+        navigate("/places")
+      })
+      .catch((error) => {
+        console.error("Error adding place:", error);
+      });
   };
 
   return (
-    <form onSubmit={handleSubmit} className="self-center shadow-lg border-solid border rounded-xl w-80 h-64 flex-col p-4">
+    <form
+      onSubmit={handleSubmit}
+      className="self-center shadow-lg border-solid border rounded-xl w-80 h-64 flex-col p-4"
+    >
       <h2 className="text-center text-lg mb-1 -mt-0.5">Add a new Place</h2>
 
       <div className="flex justify-between p-1">
         <label className="text-sm">Image: </label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="url"
           name="image"
           placeholder="Place Image"
@@ -57,7 +67,7 @@ function AddPlace({ onAddPlace }) {
       <div className="flex justify-between gap-16 p-1">
         <label className="text-sm">City: </label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="text"
           name="city"
           required
@@ -70,7 +80,7 @@ function AddPlace({ onAddPlace }) {
       <div className="flex justify-between gap-10 p-1">
         <label className="text-sm">Country: </label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="text"
           name="country"
           required
@@ -83,7 +93,7 @@ function AddPlace({ onAddPlace }) {
       <div className="flex justify-between gap-4 p-1">
         <label className="text-sm">Description: </label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="text"
           name="description"
           required
@@ -96,7 +106,7 @@ function AddPlace({ onAddPlace }) {
       <div className="flex justify-between gap-10 p-1">
         <label className="text-sm">Activity:</label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="text"
           name="activity"
           required
@@ -109,7 +119,7 @@ function AddPlace({ onAddPlace }) {
       <div className="flex justify-between gap-8 p-1">
         <label className="text-sm">Highlight: </label>
         <input
-        className="text-sm"
+          className="text-sm"
           type="text"
           name="highlight"
           required
@@ -120,7 +130,8 @@ function AddPlace({ onAddPlace }) {
       </div>
 
       <button className="bg-cyan-600 hover:bg-cyan-800 text-white font-bold py-1 px-2 rounded ml-28 mt-1.5 text-sm">
-        Add</button>
+        Add
+      </button>
     </form>
   );
 }
