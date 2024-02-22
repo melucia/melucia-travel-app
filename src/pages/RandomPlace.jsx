cadb94ef49f49c67ffc0001d30a98300377b2a57
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-22 14:31:46 +0100

    styling tailwind randomplace

--- src/pages/RandomPlace.jsx
+++ src/pages/RandomPlace.jsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import RandomButton from "../components/RandomButton";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 function RandomPlace() {
   const [randomPlaces, setRandomPlaces] = useState(null);
@@ -45,47 +41,45 @@ function RandomPlace() {
       </h2>
       <RandomButton onClick={handleClick} />
       {!onePlace && (
-        <div className="flex justify-center items-center h-96">
-          <h2 className="text-semibold text-2xl">Click the button to start!</h2>
+        <div className="flex justify-center items-start h-96">
+          <h2 className="text-semibold text-xl">Click the button to start!</h2>
         </div>
       )}
       {onePlace && (
         <div className="flex justify-center items-center">
-          <div className="w-3/4">
-            <Card className="w-full ">
-              <CardContent className="flex flex-col gap-4">
-                <div className="flex flex-col items-center">
-                  <span className="text-xl lg:text-3xl font-semibold">
-                    {onePlace.city}
-                  </span>
-                  <img
-                    src={onePlace.image}
-                    className="h-full w-1/2  rounded-md object-cover mt-4"
-                  />
+          <Card className="w-1/2 ">
+            <CardContent className="flex flex-col gap-4">
+              <div className="flex flex-col items-center">
+                <span className="text-xl lg:text-3xl font-semibold">
+                  {onePlace.city}
+                </span>
+                <img
+                  src={onePlace.image}
+                  className="h-full w-1/2  rounded-md object-cover mt-4"
+                />
+              </div>
+              <div className="flex flex-col justify-center items-center text-center gap-4">
+                <span className="text-lg lg:text-xl">{onePlace.country}</span>
+                <span className="text-md lg:text-lg">
+                  Activity: {onePlace.activity}
+                </span>
+                <span className="text-md lg:text-lg">
+                  Highlight: {onePlace.highlight}
+                </span>
+                <span className="text-lg">{onePlace.description}</span>
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-row justify-center items-center">
+              <div className="flex justify-start gap-8">
+                <div className="border rounded-xl px-2 bg-emerald-300 text-md">
+                  #{onePlace.tagOne}
                 </div>
-                <div className="flex flex-col justify-center items-center text-center gap-4">
-                  <span className="text-lg lg:text-xl">{onePlace.country}</span>
-                  <span className="text-md lg:text-lg">
-                    Activity: {onePlace.activity}
-                  </span>
-                  <span className="text-md lg:text-lg">
-                    Highlight: {onePlace.highlight}
-                  </span>
-                  <span className="text-lg">{onePlace.description}</span>
+                <div className="border rounded-xl px-2 bg-emerald-300 text-md">
+                  #{onePlace.tagTwo}
                 </div>
-              </CardContent>
-              <CardFooter className="flex flex-row justify-between items-center">
-                <div className="flex justify-start gap-1">
-                  <div className="border rounded-xl px-2 bg-emerald-300 text-md">
-                    #{onePlace.tagOne}
-                  </div>
-                  <div className="border rounded-xl px-2 bg-emerald-300 text-md">
-                    #{onePlace.tagTwo}
-                  </div>
-                </div>
-              </CardFooter>
-            </Card>
-          </div>
+              </div>
+            </CardFooter>
+          </Card>
         </div>
       )}
     </div>
